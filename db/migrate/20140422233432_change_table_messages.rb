84f9c6a632f0a697ea5a09ee2e727c81d4be5ca8
Nathan Grey & Marty Fajardo <student@epicodus.com>
2014-04-22 16:39:53 -0700

    add a user_id to the message model

---
+++ db/migrate/20140422233432_change_table_messages.rb
@@ -0,0 +1,5 @@
+class ChangeTableMessages < ActiveRecord::Migration
+  def change
+    add_column :messages, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421185508) do
+ActiveRecord::Schema.define(version: 20140422233432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140421185508) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
