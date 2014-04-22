4ba136fa82012b91157b9ed2e6aaa3095d1b6f29
Nathan Grey & Marty Fajardo <student@epicodus.com>
2014-04-21 17:31:11 -0700

    adds functionality to send a message to multiple numbers

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,14 +5,18 @@ class Message < ActiveRecord::Base
 private
 
   def send_message
-    response = RestClient::Request.new(
-        :method => :post,
-        :url => 'https://api.twilio.com/2010-04-01/Accounts/AC353156de897840b21a77585f34394055/Messages.json',
-        :user => ENV['TWILIO_ACCOUNT_SID'],
-        :password => ENV['TWILIO_AUTH_TOKEN'],
-        :payload => { :Body => body,
-                      :From => from,
-                      :To => to}
-      ).execute
+
+    numbers = to.split(",")
+    numbers.each do |number|
+
+      response = RestClient::Request.new(
+          :method => :post,
+          :url => 'https://api.twilio.com/2010-04-01/Accounts/AC353156de897840b21a77585f34394055/Messages.json',
+          :user => ENV['TWILIO_ACCOUNT_SID'],
+          :password => ENV['TWILIO_AUTH_TOKEN'],
+          :payload => { :Body => body,
+                        :From => from,
+                        :To => number}
+        ).execute
   end
 end
